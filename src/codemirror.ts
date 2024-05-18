385d44c37dd807452047dd77c44a671e93aa59be
Lincoln Lima <lincoln.lima@ufms.br>
2024-05-18 02:08:21 -0400

    melhorias de integração do editor

--- src/codemirror.ts
+++ src/codemirror.ts
@@ -1,31 +1,38 @@
-import {EditorState} from "@codemirror/state"
+// import {EditorState} from "@codemirror/state"
 import {EditorView, keymap} from "@codemirror/view"
 import {defaultKeymap} from "@codemirror/commands"
-import {dracula} from 'thememirror';
+import {espresso} from 'thememirror';
 
-let startState = EditorState.create({
+/* let startState = EditorState.create({
     doc: "Hello World",
     extensions: [
         keymap.of(defaultKeymap),
         dracula
     ]
-})
+}) */
 
-const editor_area = document.querySelector("#editor-area");
+const editor_area = document.getElementById("area-texto");
 
-/* function editorFromTextArea(textarea, extensions) {
-    let view = new EditorView({doc: textarea.value, extensions})
+function editorFromTextArea(textarea) {
+    let view = new EditorView({
+        doc: textarea.value,
+        extensions: [
+            keymap.of(defaultKeymap),
+            espresso
+        ]
+    })
+
     textarea.parentNode.insertBefore(view.dom, textarea)
     textarea.style.display = "none"
     if (textarea.form) textarea.form.addEventListener("submit", () => {
       textarea.value = view.state.doc.toString()
     })
     return view
-} */
+}
 
-// let view = editorFromTextArea(editor_area, startState.extensions)
+let view = editorFromTextArea(editor_area)
 
-let view = new EditorView({
+/* let view = new EditorView({
     state: startState,
     parent: editor_area
-})
+}) */
