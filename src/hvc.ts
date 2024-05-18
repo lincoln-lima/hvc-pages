c210700c809e646bc1393297568aa878365802b9
Lincoln Lima <lincoln.lima@ufms.br>
2024-05-18 02:07:26 -0400

    adicionado evento de click para executar programa do editor

--- src/hvc.ts
+++ src/hvc.ts
@@ -1,13 +1,21 @@
 import { HVC } from "hvcjs"
 
-const hvc = new HVC()
+const hvc = new HVC();
 
+/* evento de click para executar código */
+const executar = document.getElementById("run");
+executar.addEventListener('click', () => {
+    /* recuperando código da área de texto */
+    var codigo = document.getElementById("area-texto");
+    hvc.setCode(codigo.value);
+
+    hvc.run();
+});
+
+/* evento de saída */
 hvc.addEventOutput((out:string) => {
-
-    console.log(out);
-
+    var saida = document.getElementById("saida");
+    
+    saida.innerText = out;
+    // console.log(out);
 })
-
-hvc.setCode("740 840 000 20")
-
-hvc.run()
