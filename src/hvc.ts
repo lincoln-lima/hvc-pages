6c321692c0b6dbd311e540373a090221ebb0de74
Lincoln Lima <lincoln.lima@ufms.br>
2024-05-19 02:20:41 -0400

    avançando integração do módulo HVC

--- src/hvc.ts
+++ src/hvc.ts
@@ -1,21 +1,28 @@
-import { HVC } from "hvcjs"
+import { HVC, HVM, HVMState } from "hvcjs"
 
 const hvc = new HVC();
 
 /* evento de click para executar código */
-const executar = document.getElementById("run");
+const executar = document.getElementById("run")!;
 executar.addEventListener('click', () => {
     /* recuperando código da área de texto */
-    var codigo = document.getElementById("area-texto");
+    var codigo = document.getElementById("area-texto")!;
     hvc.setCode(codigo.value);
 
-    hvc.run();
+    hvc.debug(100);
 });
 
 /* evento de saída */
 hvc.addEventOutput((out:string) => {
-    var saida = document.getElementById("saida");
+    var saida = document.getElementById("saida")!;
     
     saida.innerText = out;
     // console.log(out);
+})
+
+hvc.addEventClock((HVM:HVMState) => {
+    const hvm = hvc.getHVM();
+
+    console.log(hvm.calculadora.getAcumulador());
+    console.log(hvm.epi.lerRegistro());
 })
