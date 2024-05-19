200969b2e75404e892b595b9a588314f52e26c27
Lincoln Lima <lincoln.lima@ufms.br>
2024-05-19 18:04:44 -0400

    [ADD] integração dos valores dos conteúdos das gavetas, reorganização e substituindo var por let

--- src/hvc.ts
+++ src/hvc.ts
@@ -1,28 +1,68 @@
-import { HVC, HVM, HVMState } from "hvcjs"
+import { HVC } from "hvcjs"
 
 const hvc = new HVC();
 
-/* evento de click para executar código */
-const executar = document.getElementById("run")!;
-executar.addEventListener('click', () => {
-    /* recuperando código da área de texto */
-    var codigo = document.getElementById("area-texto")!;
-    hvc.setCode(codigo.value);
+/* seções da página */
 
-    hvc.debug(100);
+// áreas de texto
+const saida = document.getElementById("saida")!;
+const acumulador = document.getElementById("acumulador")!;
+const epi = document.getElementById("epi")!;
+
+/* 
+// seções com itens
+const gaveteiro = document.getElementById("secs-gaveteiro")!;
+*/
+
+/* botões da página - ações de clique */
+const executar = document.getElementById("run")!;
+
+function rodarCodigo() {
+    /* recuperando código da área de texto */
+    let codigo = document.getElementById("area-texto")!;
+    hvc.setCode(codigo.innerHTML);
+
+    hvc.debug(700);
+}
+
+/* evento de click para executar código */
+executar.addEventListener('click', () => {
+    rodarCodigo();
 });
 
-/* evento de saída */
+/* evento de atalho */
+document.addEventListener('keydown', e => {
+    if(e.key.toLocaleLowerCase() === "f9") rodarCodigo();
+    // console.log(e);
+})
+
+/* evento de saída do HVC */
 hvc.addEventOutput((out:string) => {
-    var saida = document.getElementById("saida")!;
-    
     saida.innerText = out;
     // console.log(out);
 })
 
-hvc.addEventClock((HVM:HVMState) => {
+hvc.addEventClock(HVMState => {
     const hvm = hvc.getHVM();
 
-    console.log(hvm.calculadora.getAcumulador());
-    console.log(hvm.epi.lerRegistro());
+    const val_acumulador = hvm.calculadora.getAcumulador().toString();
+    const val_epi = hvm.epi.lerRegistro().toString();
+    const val_gaveta = hvm.gaveteiro.getGavetas();
+
+    acumulador.innerText = val_acumulador;
+    epi.innerText = val_epi;
+
+    for(let i = 0; i < 100; i++) {
+        let cont_gaveta = document.getElementById("cont-gaveta-"+i)!;
+        
+        cont_gaveta.innerText = val_gaveta[i] ? val_gaveta[i].toString() : "---";
+        // let cont_gaveta = gavetas[i].getElementsByClassName("cont-gaveta");
+        // console.log(cont_gaveta);
+    }
+
+    // console.log("-------------");
+    
+    // console.log(val_gaveta);
+    // console.log("Acumulador: " + val_acumulador);
+    // console.log("EPI: " + val_epi);
 })
