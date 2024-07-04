4192c36672b9f04cec4e17fd640c20130df59754
Lincoln Lima <lincoln.lima@ufms.br>
2024-07-04 16:20:05 -0400

    [UP] ajustes gerais com relação a cabeçalho e responsividade

--- public/js/tela.js
+++ public/js/tela.js
@@ -1,6 +1,14 @@
-function monitoraTela() {
+function monitoraTelaIndex() {
     menu.style.opacity = window.innerWidth > 1064 ? 1 : 0;
     menu.style.visibility = window.innerWidth > 1064 ? "visible" : "hidden";
+
+    // if(window.innerWidth > 1064) menu.style.transition = "none";
+    // menu.style.transition = window.innerWidth > 1064 ? "none" : "0.5s";
+}
+
+function monitoraTelaPlayground() {
+    menu.style.opacity = window.innerWidth > 939 ? 1 : 0;
+    menu.style.visibility = window.innerWidth > 939 ? "visible" : "hidden";
 
     // if(window.innerWidth > 1064) menu.style.transition = "none";
     // menu.style.transition = window.innerWidth > 1064 ? "none" : "0.5s";
