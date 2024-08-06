e3c123f881c772edba8061e7bfa93038adada110
Lincoln Lima <lincoln.lima@ufms.br>
2024-08-06 16:02:36 -0400

    [FIX] readequações do merge

--- js/copiar.js
+++
@@ -1,7 +0,0 @@
-function copiarComando(id) {
-    var copyText = document.getElementById(id).innerHTML;
-
-    navigator.clipboard.writeText(copyText);
-
-    alert("Copiado para área de transferência:\n" + copyText);
-}
--- js/tela.js
+++
@@ -1,7 +0,0 @@
-function monitoraTela() {
-    menu.style.opacity = window.innerWidth > 1064 ? 1 : 0;
-    menu.style.visibility = window.innerWidth > 1064 ? "visible" : "hidden";
-
-    // if(window.innerWidth > 1064) menu.style.transition = "none";
-    // menu.style.transition = window.innerWidth > 1064 ? "none" : "0.5s";
-}
