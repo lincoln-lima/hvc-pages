9af4e8e813fe879cddc31935e79ae3ff71df42ce
Lincoln Lima <lincoln.lima@ufms.br>
2024-10-09 20:54:41 -0400

    [FIX] corrigindo páginas a serem resolvidas

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,14 @@ export default defineConfig({
       input: {
         main: resolve('./index.html'),
         documentation: resolve('./pages/documentation.html'),
-        playground: resolve('./pages/playground.html')
+        playground: resolve('./pages/playground.html'),
+        card: resolve('./templates/modal/card.html'),
+        configs: resolve('./templates/modal/configs.html'),
+        error: resolve('./templates/modal/error.html'),
+        help: resolve('./templates/modal/help.html'),
+        footer: resolve('./templates/footer.html'),
+        table: resolve('./templates/modal/table.html'),
+        drawer: resolve('./templates/playground/drawer.html')
       }
     }
   },
