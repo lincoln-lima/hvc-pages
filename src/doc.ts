d1f4a0f35e4a27434a87de9d381c8d539aeec767
Lincoln Lima <lincoln.lima@ufms.br>
2024-10-30 18:57:13 -0400

    [UP] início do menu sticky responsivo

--- src/doc.ts
+++ src/doc.ts
@@ -1,6 +1,19 @@
 import { globals } from "./default";
 // ------------------------------------------------------------------------------- 
-import "/src/styles/documentation/documentation.scss"
+import "/src/styles/documentation/documentation.scss";
 // ------------------------------------------------------------------------------- 
 window.addEventListener('resize', () => globals.actions.monitoreMenu(900));
 window.addEventListener('load', () => globals.actions.monitoreMenu(900));
+// ------------------------------------------------------------------------------- 
+const shownav = document.getElementById("show-nav")!;
+const stickynav = document.getElementById("sticky-nav")!;
+
+const switchnav = () => {
+    const rightnav = stickynav.style['right'];
+    const rightshow = shownav.style['right'];
+    
+    stickynav.style['right'] = rightnav == '0' || rightnav == '' ? '0' : "revert-layer";
+    shownav.style['right'] = rightshow == '0' || rightshow == '' ? '0' : stickynav.style['width'];
+};
+
+shownav.addEventListener('click', switchnav);
